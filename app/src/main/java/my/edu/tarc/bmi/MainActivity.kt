63f01b7431bcc31061c9a5d4a0b3b0a463b2b1f7
KONKumi <loxh-wm20@student.tarc.edu.my>
2023-02-27 11:37:07 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/bmi/MainActivity.kt
@@ -0,0 +1,63 @@
+package my.edu.tarc.bmi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageViewBMI: ImageView = findViewById(R.id.imageViewBMI)
+        val textViewBMI: TextView = findViewById(R.id.textViewBMI)
+        val textViewStatus: TextView = findViewById(R.id.textViewStatus)
+        val editTextWeight: EditText = findViewById(R.id.editTextWeight)
+        val editTextHeight: EditText = findViewById(R.id.editTextHeight)
+        val buttonCalculate: Button = findViewById(R.id.buttonCalculate)
+        val buttonReset: Button = findViewById(R.id.buttonReset)
+
+        buttonCalculate.setOnClickListener {
+            if (editTextWeight.text.isBlank()) {
+                editTextWeight.setError(getString(R.string.value_required))
+                return@setOnClickListener
+            }
+
+            if (editTextHeight.text.isBlank()) {
+                editTextHeight.setError(getString(R.string.value_required))
+                return@setOnClickListener
+            }
+
+            var weight = editTextWeight.text.toString().toFloat()
+            var height = editTextHeight.text.toString().toFloat()
+            var bmi = weight / (height / 100).pow(2)
+            if (bmi < 18.5)
+            {
+                imageViewBMI.setImageResource(R.drawable.under)
+            textViewBMI.text = String.format("%s : %.2f", getString(R.string.bmi), bmi)
+            textViewStatus.text = String.format("%s : %s", getString(R.string.status), getString(R.string.under))
+            }
+            else if (bmi < 25){
+                imageViewBMI.setImageResource(R.drawable.normal)
+                textViewBMI.text = String.format("%s : %.2f", getString(R.string.bmi), bmi)
+                textViewStatus.text = String.format("%s : %s", getString(R.string.status), getString(R.string.normal))
+            }
+            else{
+                imageViewBMI.setImageResource(R.drawable.over)
+                textViewBMI.text = String.format("%s : %.2f", getString(R.string.bmi), bmi)
+                textViewStatus.text = String.format("%s : %s", getString(R.string.status), getString(R.string.over))
+            }
+        }
+        buttonReset.setOnClickListener{
+            editTextHeight.text.clear()
+            editTextWeight.text.clear()
+            textViewBMI.text = ""
+            textViewStatus.text = ""
+            imageViewBMI.setImageResource(R.drawable.empty)
+        }
+    }
+}
